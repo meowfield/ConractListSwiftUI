50f0c0efd66d3812924fab502fb962d666129d03
meowfield <meowfield@yandex.ru>
2024-10-22 16:28:38 +0200

    Initial Commit

---
+++ ConractListSwiftUI/ConractListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConractListSwiftUIApp.swift
+//  ConractListSwiftUI
+//
+//  Created by Данис Гаязов on 22.10.24..
+//
+
+import SwiftUI
+
+@main
+struct ConractListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConractListSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConractListSwiftUI
+//
+//  Created by Данис Гаязов on 22.10.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
